d61392ec09a8338d4ab1024ffd7fecce503eddfc
Collin K. Cusce <collin@avalabs.org>
2020-01-24 13:22:05 -0500

    small fix

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ let s:slopes.Slopes = new slopes.Slopes("localhost", 9650);
 
 let utxoset:slopes.UTXOSet = new slopes.UTXOSet();
 
-let u:Array<slopes.UTXO>;
+let u:Array<slopes.UTXO> = [];
 
 u.push(new slopes.UTXO());
 
@@ -39,3 +39,15 @@ let utc:BN = slopes.UnixNow();
 let po:slopes.PersistanceOptions = new slopes.PersistanceOptions("a", false, "union");
 
 let avm:slopes.AVM = new slopes.AVM(s);
+
+
+let address1 = "c3344128e060128ede3523a24a461c8943ab0859";
+let address2 = "51025c61fbcfc078f69334f834be6dd26d55a955";
+
+console.log(
+    "address1", bintools.avaSerialize(Buffer.from(address1, "hex")),
+    "address2", bintools.avaSerialize(Buffer.from(address2, "hex"))
+    );
+
+    let mypk = Buffer.from("d0e17d4b31380f96a42b3e9ffc4c1b2a93589a1e51d86d7edc107f602fbc7475", "hex");
+console.log("pk58", bintools.avaSerialize(mypk));
